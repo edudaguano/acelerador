7a6a9936295cb6cef253db265c95119291f3a5ed
Eduardo Daguano <edu.daguano@gmail.com>
2020-02-06 18:16:03 -0300

    Initial commit

---
+++ src/main/java/com/br/accava/acelerador/AceleradorApplication.java
@@ -0,0 +1,13 @@
+package com.br.accava.acelerador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceleradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/accava/acelerador/AceleradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.accava.acelerador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceleradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
